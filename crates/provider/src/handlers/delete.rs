d125dd18349af1c143e1633ad6ebe8e3e5295fbc
scutKKsix <1129332011@qq.com>
2025-05-07 07:32:09 +0000

    netns

--- crates/provider/src/handlers/delete.rs
+++ crates/provider/src/handlers/delete.rs
@@ -18,7 +18,6 @@ pub async fn delete_handler(info: web::Json<DeleteContainerInfo>) -> impl Respon
         .unwrap_or_else(|| consts::DEFAULT_FUNCTION_NAMESPACE.to_string());
 
     let namespaces = ContainerdManager::list_namespaces().await.unwrap();
-    log::info!("{:?}", namespaces);
     if !namespaces.contains(&namespace.to_string()) {
         return HttpResponse::NotFound().body(format!("Namespace '{}' does not exist", namespace));
     }
